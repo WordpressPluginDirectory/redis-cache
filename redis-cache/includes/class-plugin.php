140700a23c6dec5eb805c3b8cc62534ee8c7e75c
Justin Gardner <justin@rhynorater.com>
2025-07-25 06:02:50 -0500

    2.6.3

--- redis-cache/includes/class-plugin.php
+++ redis-cache/includes/class-plugin.php
@@ -333,7 +333,12 @@ class Plugin {
             return;
         }
 
-        wp_enqueue_style( 'redis-cache', WP_REDIS_PLUGIN_DIR . '/assets/css/admin.css', [], WP_REDIS_VERSION );
+        wp_enqueue_style(
+            'redis-cache',
+            trailingslashit( WP_REDIS_PLUGIN_DIR ) . 'assets/css/admin.css',
+            [],
+            WP_REDIS_VERSION
+        );
     }
 
     /**
@@ -822,8 +827,9 @@ class Plugin {
      * @return string
      */
     protected function admin_bar_style() {
+        // phpcs:disable Squiz.PHP.Heredoc.NotAllowed
         return <<<HTML
-            <style>
+            <style id="redis-cache-admin-bar-style">
                 #wpadminbar ul li.redis-cache-error {
                     background: #b30000;
                 }
@@ -847,7 +853,7 @@ HTML;
         $flushMessage = __( 'Flushing cache...', 'redis-cache' );
 
         return <<<HTML
-            <script>
+            <script id="redis-cache-admin-bar">
                 (function (element) {
                     if (! element) {
                         return;
@@ -1071,15 +1077,13 @@ HTML;
      * @return void
      */
     public function ajax_flush_cache() {
-        if ( ! wp_verify_nonce( $_POST['nonce'] ) ) {
-            $message = 'Invalid Nonce.';
+        if ( ! wp_verify_nonce( $_POST['nonce'] ?? '' ) ) { // phpcs:ignore WordPress.Security.ValidatedSanitizedInput
+            wp_die( esc_html__( 'Invalid Nonce.', 'redis-cache' ) );
         } else if ( wp_cache_flush() ) {
-            $message = 'Object cache flushed.';
+            wp_die( esc_html__( 'Object cache flushed.', 'redis-cache' ) );
         } else {
-            $message = 'Object cache could not be flushed.';
+            wp_die( esc_html__( 'Object cache could not be flushed.', 'redis-cache' ) );
         }
-
-        wp_die( __( $message , 'redis-cache' ) );
     }
 
     /**
@@ -1595,6 +1599,7 @@ HTML;
      * @return void
      */
     public function litespeed_disable_objectcache() {
+        // phpcs:ignore WordPress.Security.NonceVerification.Missing
         if ( isset( $_POST['LSCWP_CTRL'], $_POST['LSCWP_NONCE'], $_POST['object'] ) ) {
             $_POST['object'] = '0';
         }
--- redis-cache/includes/class-predis.php
+++ redis-cache/includes/class-predis.php
@@ -206,13 +206,31 @@ class Predis {
         $cluster = array_values( WP_REDIS_CLUSTER );
 
         foreach ( $cluster as $key => $server ) {
-            $connection_string = parse_url( $server );
+            // phpcs:ignore WordPress.WP.AlternativeFunctions.parse_url_parse_url
+            $components = parse_url( $server );
 
-            $cluster[ $key ] = sprintf(
-                "%s:%s",
-                $connection_string['host'],
-                $connection_string['port']
-            );
+            if ( ! empty( $components['scheme'] ) ) {
+                $scheme = $components['scheme'];
+            } elseif ( defined( 'WP_REDIS_SCHEME' ) ) {
+                $scheme = WP_REDIS_SCHEME;
+            } else {
+                $scheme = null;
+            }
+
+            if ( isset( $scheme ) ) {
+                $cluster[ $key ] = sprintf(
+                    '%s://%s:%d',
+                    $scheme,
+                    $components['host'],
+                    $components['port']
+                );
+            } else {
+                $cluster[ $key ] = sprintf(
+                    '%s:%d',
+                    $components['host'],
+                    $components['port']
+                );
+            }
         }
 
         return $cluster;
